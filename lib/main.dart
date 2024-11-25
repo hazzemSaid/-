0716a988a9fb7ec31c038ec809d2882f8d7759c8
haazemsaid <haazemsaidd@gmail.com>
2024-11-25 08:22:08 +0200

    remove Device Preview dependency and update main app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:quran_app/core/routes.dart';
 
 void main() {
-  runApp(DevicePreview(
-    enabled: true,
-    builder: (context) => const QuranApp(),
-  ));
+  runApp(
+    const QuranApp(),
+  );
 }
 
 class QuranApp extends StatefulWidget {
@@ -25,11 +23,7 @@ class _QuranAppState extends State<QuranApp> {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: Routes.router,
-      locale: DevicePreview.locale(context), // دعم اللغات
-      builder: DevicePreview.appBuilder,
-
       debugShowCheckedModeBanner: false,
-
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: const [
         Locale('en', ''), // English
