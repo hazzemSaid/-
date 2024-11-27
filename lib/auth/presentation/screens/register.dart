b8c5608c2336fbb7151bc287ffa6e1dcf702c322
haazemsaid <haazemsaidd@gmail.com>
2024-11-27 21:48:34 +0200

    add CustomTextField widget for improved user registration experience

--- lib/auth/presentation/screens/register.dart
+++ lib/auth/presentation/screens/register.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/CustomTextField.dart';
+
 class UserRegistrationScreen extends StatelessWidget {
   const UserRegistrationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFFFFFFFF),
+        backgroundColor: const Color(0xFFFFFFFF),
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -87,67 +89,5 @@ class UserRegistrationScreen extends StatelessWidget {
             ],
           ),
         ));
-  }
-}
-
-class CustomTextField extends StatefulWidget {
-  const CustomTextField({
-    super.key,
-    required this.text,
-    this.isPassword = false,
-  });
-
-  final String text;
-  final bool isPassword;
-
-  @override
-  State<CustomTextField> createState() => _CustomTextFieldState();
-}
-
-class _CustomTextFieldState extends State<CustomTextField> {
-  bool isPasswordVisible = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      obscureText: widget.isPassword ? !isPasswordVisible : false,
-      textAlign: TextAlign.right,
-      decoration: InputDecoration(
-        prefixIcon: widget.isPassword
-            ? IconButton(
-                onPressed: () {
-                  setState(() {
-                    isPasswordVisible = !isPasswordVisible;
-                  });
-                },
-                icon: Icon(
-                  isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                ),
-              )
-            : null,
-        enabledBorder: const OutlineInputBorder(
-          gapPadding: 10,
-          borderSide: BorderSide(
-            color: Colors.black45,
-          ),
-          borderRadius: BorderRadius.all(
-            Radius.circular(30),
-          ),
-        ),
-        focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.green,
-            width: 2.0,
-          ),
-          borderRadius: BorderRadius.all(
-            Radius.circular(30),
-          ),
-        ),
-        hintText: widget.text,
-        hintStyle: const TextStyle(
-          color: Colors.black,
-        ),
-      ),
-    );
   }
 }
---
+++ lib/auth/presentation/widgets/CustomTextField.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  const CustomTextField({
+    super.key,
+    required this.text,
+    this.isPassword = false,
+  });
+
+  final String text;
+  final bool isPassword;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: widget.isPassword ? !isPasswordVisible : false,
+      textAlign: TextAlign.right,
+      decoration: InputDecoration(
+        prefixIcon: widget.isPassword
+            ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    isPasswordVisible = !isPasswordVisible;
+                  });
+                },
+                icon: Icon(
+                  isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                ),
+              )
+            : null,
+        enabledBorder: const OutlineInputBorder(
+          gapPadding: 10,
+          borderSide: BorderSide(
+            color: Colors.black45,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(30),
+          ),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.green,
+            width: 2.0,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(30),
+          ),
+        ),
+        hintText: widget.text,
+        hintStyle: const TextStyle(
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
